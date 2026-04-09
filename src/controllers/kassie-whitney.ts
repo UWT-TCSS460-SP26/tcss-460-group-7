53bbe8de9ae01a705f895af8571bec903190bf80
kassieRepo2 <186893026+Kjw072892@users.noreply.github.com>
2026-04-08 18:51:43 -0700

    added personal route

---
+++ src/controllers/kassie-whitney.ts
@@ -0,0 +1,24 @@
+import { Request, Response } from 'express';
+
+export const getGreeting = (request: Request, response: Response) => {
+  interface Student {
+    name: string;
+    age: number;
+    grade: string | number;
+    school: string;
+  }
+
+  const user: Student = {
+    name: 'Kassie Whitney',
+    age: 33,
+    grade: 'Senior',
+    school: 'University of Washington Tacoma',
+  };
+
+  const greeting: string = `Hello! My name is ${user.name}. I am ${user.age} years old.
+  I am currently a ${user.grade} at ${user.school}! It is nice to meet everyone!`;
+
+  response.status(200).json({
+    message: greeting,
+  });
+};
