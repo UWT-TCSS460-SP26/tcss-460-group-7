0e22912953f884136e6cdf115d7a94513fa91b78
LeDuyhungC <hle4831@gmail.com>
2026-04-10 12:10:33 -0700

    prettier format

--- src/controllers/greetingDuyHung.ts
+++ src/controllers/greetingDuyHung.ts
@@ -1,5 +1,5 @@
-import { Request, Response } from "express";
+import { Request, Response } from 'express';
 
 export const getGreeting = (_request: Request, response: Response) => {
-    response.status(200).json({ message: 'Hello, my name is Duy-Hung Le and I\'m glad to meet you!' });
-}
+  response.status(200).json({ message: "Hello, my name is Duy-Hung Le and I'm glad to meet you!" });
+};
