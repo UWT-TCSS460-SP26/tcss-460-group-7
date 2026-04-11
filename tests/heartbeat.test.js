4b52af40cefcec3a2bd119125b407be3a916a06b
kassie <kassie.j.whitney92@Gmail.com>
2026-04-10 22:04:48 -0700

    reorganized the meeting folder

--- tests/heartbeat.test.js
+++ tests/heartbeat.test.js
@@ -1,15 +1,17 @@
-"use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
-Object.defineProperty(exports, "__esModule", { value: true });
-const supertest_1 = __importDefault(require("supertest"));
-const app_1 = require("../src/app");
+'use strict';
+var __importDefault =
+  (this && this.__importDefault) ||
+  function (mod) {
+    return mod && mod.__esModule ? mod : { default: mod };
+  };
+Object.defineProperty(exports, '__esModule', { value: true });
+const supertest_1 = __importDefault(require('supertest'));
+const app_1 = require('../src/app');
 describe('Heartbeat Route', () => {
-    it('GET /status - returns the status of the client', async () => {
-        const response = await (0, supertest_1.default)(app_1.app).get('/status');
-        expect(response.status).toBe(200);
-        expect(response.body.message).toBe('Status: OK');
-    });
+  it('GET /status - returns the status of the client', async () => {
+    const response = await (0, supertest_1.default)(app_1.app).get('/status');
+    expect(response.status).toBe(200);
+    expect(response.body.message).toBe('Status: OK');
+  });
 });
 //# sourceMappingURL=heartbeat.test.js.map
