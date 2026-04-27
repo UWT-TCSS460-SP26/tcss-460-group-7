ed20749502b080cd102b58deaa1071276307cdad
Skyler Z. Broussard <skyzen888@gmail.com>
2026-04-26 21:47:52 -0700

    fix: resolve migration conflicts and repair openapi.yaml merge artifacts

--- prisma/migrations/20260426060445_updated_requirement/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[rating,authorId]` on the table `ratings` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "ratings_rating_authorId_key" ON "ratings"("rating", "authorId");
