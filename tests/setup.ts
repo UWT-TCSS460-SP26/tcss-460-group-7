8236323e2eaf7ca63c4cff14ec03ae1a23969ebd
Skyler Z. Broussard <skyzen888@gmail.com>
2026-05-01 22:08:06 -0700

    fixed test issue - formatting

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,3 +1,4 @@
 process.env.AUTH_ISSUER = 'https://tcss-460-iam.onrender.com';
 process.env.API_AUDIENCE = 'group-7-api';
-process.env.DATABASE_URL = process.env.DATABASE_URL ?? 'postgresql://dummy:dummy@localhost:5432/dummy';
+process.env.DATABASE_URL =
+  process.env.DATABASE_URL ?? 'postgresql://dummy:dummy@localhost:5432/dummy';
