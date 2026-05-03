4fbccbca6087cac73d4a964a93e39aa828fa7b7d
kassie <kassie.j.whitney92@Gmail.com>
2026-05-03 13:23:39 -0700

    updated test suite

--- tests/setup.ts
+++ tests/setup.ts
@@ -1,2 +1,3 @@
-process.env.AUTH_ISSUER = 'https://tcss-460-iam.onrender.com';
-process.env.API_AUDIENCE = 'group-7-api';
+process.env.AUTH_ISSUER ??= 'https://tcss-460-iam.onrender.com';
+process.env.API_AUDIENCE ??= 'group-7-api';
+process.env.DATABASE_URL ??= 'postgresql://dummy:dummy@localhost:5432/dummy';
